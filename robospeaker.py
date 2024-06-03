bfa84a98613bb9d296abdbf18c65b059fe8b4c07
shivang bijalwan <96715527+Codeshivang@users.noreply.github.com>
2024-06-04 02:37:39 +0530

    Add files via upload

---
+++ robospeaker.py
@@ -0,0 +1,13 @@
+import pyttsx3
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+if __name__ == "__main__":
+   while True:
+    text = input("Enter the text you want the robo speaker to say: ")
+    if text=="q":
+       break
+    speak(text)
